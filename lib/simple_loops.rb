fc7d7243281ab45fde15e36727fa2cb787221104
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-10 20:55:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -3,10 +3,6 @@ def loop_message_five_times(message)
   5.times {puts message}
 end 
 
-def loop_message_n_times(integer, message)
-  count = 0
- while count < integer do 
-   {puts message}
- end 
- count+=1
+def loop_message_n_times(message, number)
+
 end 
